45f8529cf953a88fc035ccc4a0917feaf91eba4b
LauYann <metalyanni@gmail.com>
2023-08-02 17:05:32 -0600

    add botou para encender y apagar

--- Linterna/ContentView.swift
+++ Linterna/ContentView.swift
@@ -8,14 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var lamp = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        VStack{
+            ZStack{
+                Color(lamp ? .black : .white)
+                
+                Button(action:  {
+                    lamp.toggle()
+                }){
+                    Text("encender")
+                }
+            }
+            
+        }.edgesIgnoringSafeArea(.all)
+        
     }
 }
 
