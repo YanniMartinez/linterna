0a35d4cd869f5fd6a98eb360f3d8fd571ee3cc10
LauYann <metalyanni@gmail.com>
2023-08-02 16:40:06 -0600

    Initial Commit

---
+++ Linterna/LinternaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinternaApp.swift
+//  Linterna
+//
+//  Created by MacBook 26 on 02/08/23.
+//
+
+import SwiftUI
+
+@main
+struct LinternaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
